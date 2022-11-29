79dc00033f7ad711b8c380e3d38a167c5b218e20
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-11-29 14:16:56 -0300

    chore: reorganizando comentários

--- pilha/pilha.cpp
+++ pilha/pilha.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 class Node{
-    // A classe Node é amiga da classe Pilha, isso significa que a classe node pode acessar campos privados e protegidos da classe Pilha, além disso é uma classe util a classe pilha pois dela virá o encadeamento que será utilizado na pilha.
+    // A classe Node é amiga da classe Pilha. 
+    // Isso significa que a classe node pode acessar campos privados e protegidos da classe Pilha,
+    // além disso é uma classe util a classe pilha pois dela virá o encadeamento que será utilizado na pilha.
     friend class Pilha;
     private:
-        // No campo privado definimos os atributos da classe Node, o dado que foi guardado no campo atual e qual o campo seguinte gerando um encadeamento similar ao dá lista ligada.
+        // No campo privado definimos os atributos da classe Node, 
+        // o dado que foi guardado no campo atual e qual o campo seguinte gerando um encadeamento similar ao dá lista ligada.
         int node_dado;
         Node* node_proximo;
     public:
-        // No campo publico definimos uma função do construtor que pode ser acessada de qualquer lugar que instancie a classe o construtor garante a atribuição de todo os valores da classe para que seja utilizado corretamente.
+        // No campo publico definimos uma função do construtor que pode ser acessada de qualquer lugar que instancie a classe.
+        // O construtor garante a atribuição de todo os valores da classe para que seja utilizado corretamente.
         Node(int dado){
             node_dado = dado;
             node_proximo = NULL;
@@ -18,23 +22,28 @@ class Node{
 };
 
 class Pilha{
-    // A classe Pilha necessita de 1 campo essencial e 1 campo opcional, sendo o campo essencial chamado de Topo. Que identifica o ponto inicial da pilha e de onde deve começar a ser lindo quando for desempilhado.
+    // A classe Pilha necessita de 1 campo essencial e 1 campo opcional, sendo o campo essencial chamado de Topo. 
+    // Que identifica o ponto inicial da pilha e de onde deve começar a ser lindo quando for desempilhado.
     private:
         Node* topo;
         unsigned int tamanho;
     public:
-        // Definimos uma função chamada limpaPilha para limpar a pilha após o uso, isso é uma função opcional que é usada no Destrutor mas, também pode ter seu método implementado diretamente no destructor.
+        // Definimos uma função chamada limpaPilha para limpar a pilha após o uso, 
+        // isso é uma função opcional que é usada no Destrutor mas, 
+        // também pode ter seu método implementado diretamente no destructor.
         void limpaPilha(){
             while (tamanho > 0){
                 desempilha();
             };
         }
-        // aqui é definido o construtor da classe Pilha, inicializando os valores da Pilha ao instacia-la.
+        // aqui é definido o construtor da classe Pilha, 
+        // inicializando os valores da Pilha ao instacia-la.
         Pilha(){
             topo = NULL;
             tamanho = 0;
         };
-        // aqui é definido o destructor da classe Pilha, deletando todos os valores da pilha.
+        // aqui é definido o destructor da classe Pilha, 
+        // deletando todos os valores da pilha.
         ~Pilha(){
             limpaPilha();
         };
@@ -66,7 +75,9 @@ class Pilha{
 };
 
 
-// Função principal que aloca valores até chegar a um número negativo, e depois desaloca começando do ultimmo para o começo Fifo
+// Função principal que aloca valores até chegar a um número negativo, 
+// e depois desaloca começando do ultimmo para o começo Fifo
+
 int main(){
     Pilha pilha;
     int num;
